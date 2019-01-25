1d032abf61c214aecf673b910a7fc5537d754d2a
Jan Hutar <jhutar@redhat.com>
2019-01-25 13:38:12 +0100

    adhoc-scripts/compare_stats_from_grafana.py: More experiments with the tool I have forgot to commit before

--- adhoc-scripts/compare_stats_from_grafana.py
+++ adhoc-scripts/compare_stats_from_grafana.py
@@ -27,16 +27,26 @@ logging.debug("Arguments: %s" % args)
 data_first = json.load(args.first_file)
 data_second = json.load(args.second_file)
 
-table_header = ['metric', 'min', 'max', 'mean', 'median', 'int_per_dur', 'pstdev', 'pvariance', 'duration']
+# Some stats are useless
+###table_header = ['metric', 'min', 'max', 'mean', 'median', 'int_per_dur', 'pstdev', 'pvariance', 'duration']
+table_header_metric = ['metric']
+table_header_items = ['max', 'mean', 'median', 'duration']
+table_header = table_header_metric
+for factor in table_header_items:
+    table_header.append("%s change" % factor)
+    table_header.append("%s change [%%]" % factor)
+
 table_data = []
 for metric in data_first:
     table_row = [metric]
-    for factor in table_header[1:]:
+    for factor in table_header_items:
         logging.debug("Processing %s -> %s" % (metric, factor))
         value_first = data_first[metric][factor]
         value_second = data_second[metric][factor]
+        value_diff = float(value_second - value_first)
+        table_row.append("%.1f" % value_diff)
         try:
-            table_row.append("%.1f" % (float(value_second - value_first) / value_first * 100))
+            table_row.append("%.1f" % (value_diff / value_first * 100))
         except ZeroDivisionError:
             table_row.append('Err')
     table_data.append(table_row)
@@ -46,4 +56,4 @@ if args.csv:
     spamwriter.writerow(table_header)
     spamwriter.writerows(table_data)
 else:
-    print(tabulate.tabulate(table_data, headers=table_header))
+    print(tabulate.tabulate(table_data, headers=table_header, floatfmt='.1f'))
